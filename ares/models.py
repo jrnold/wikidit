e762ee70d70e26de6b4749fc8d18213dda038da6
jrnold <jeffrey.arnold@gmail.com>
2018-09-28 15:56:19 +0000

    misc

--- ares/models.py
+++ ares/models.py
@@ -171,6 +171,17 @@ class WikiPage:
         for x in binary_variables:
             yield (x, page.set_value(*x))
 
+WP10_LABELS = ("Stub", "Start", "C", "B", "A", "GA", "FA")
+"""Wikipeda WP10 Quality labels"""
+ 
+
+def load_wp10(input_file):
+    """Load wp10 data with features from a csv file"""
+    revisions = pd.read_csv(input_file, index_col='revid')
+    revisions['wp10'] = pd.Categorical(revisions['wp10'],
+                                    categories=WP10_LABELS, ordered=True)
+    return revisions
+    
 def sort_wp10(x, classes):
     classes = list(classes)
     return sorted(list(zip(classes, x)), key=lambda x: WP10_LABELS.index(x[0]))
